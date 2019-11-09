7036dbd00134fab4afb0268e161221856395e24a
RangaReddyV <36688572+RangaReddyV@users.noreply.github.com>
2019-11-09 14:29:21 +0530

    teee

--- hack2hire/src/main/java/com/dbs/hack2hire/entity/EXDEntity.java
+++ hack2hire/src/main/java/com/dbs/hack2hire/entity/EXDEntity.java
@@ -1,5 +1,117 @@
 package com.dbs.hack2hire.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="ext")
 public class EXDEntity {
 
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column(name= "Exchange_Name")
+	private String exchangeName;
+	
+	@Column(name= "Unique_Identifier")
+	private String uniqueIdentifier;
+	
+	@Column(name= "EQUITY_NAME")
+	private String equityName;
+	
+	@Column(name= "Quantity")
+	private String quantity;
+	
+	@Column(name= "TRADE_TYPE")
+	private String tradeType;
+	
+	@Column(name= "intraDay")
+	private String intraDay;
+	
+	@Column(name= "price")
+	private String price;
+	
+	@Column(name= "DEAL_ID")
+	private String dealId;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getExchangeName() {
+		return exchangeName;
+	}
+
+	public void setExchangeName(String exchangeName) {
+		this.exchangeName = exchangeName;
+	}
+
+	public String getUniqueIdentifier() {
+		return uniqueIdentifier;
+	}
+
+	public void setUniqueIdentifier(String uniqueIdentifier) {
+		this.uniqueIdentifier = uniqueIdentifier;
+	}
+
+	public String getEquityName() {
+		return equityName;
+	}
+
+	public void setEquityName(String equityName) {
+		this.equityName = equityName;
+	}
+
+	public String getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getTradeType() {
+		return tradeType;
+	}
+
+	public void setTradeType(String tradeType) {
+		this.tradeType = tradeType;
+	}
+
+	public String getIntraDay() {
+		return intraDay;
+	}
+
+	public void setIntraDay(String intraDay) {
+		this.intraDay = intraDay;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getDealId() {
+		return dealId;
+	}
+
+	public void setDealId(String dealId) {
+		this.dealId = dealId;
+	}
+	
+	
+	
+	
 }
+
--- hack2hire/src/main/java/com/dbs/hack2hire/entity/FIDEntity.java
+++ hack2hire/src/main/java/com/dbs/hack2hire/entity/FIDEntity.java
@@ -1,5 +1,107 @@
 package com.dbs.hack2hire.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name ="fid")
 public class FIDEntity {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name= "TRADEID")
+	private String tradeId;
+	
+	@Column(name= "StockName")
+	private String stockName;
+	
+	@Column(name= "ISIN")
+	private String ISIN;
+	
+	@Column(name= "Quantity")
+	private int quantity;
+	
+	@Column(name= "Buy_Sell_Indicator")
+	private String bulSellIndicator;
+	
+	@Column(name= "price")
+	private String price;
+	
+	@Column(name= "Exchange_Name")
+	private String exchangeName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTradeId() {
+		return tradeId;
+	}
+
+	public void setTradeId(String tradeId) {
+		this.tradeId = tradeId;
+	}
+
+	public String getStockName() {
+		return stockName;
+	}
+
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
+	}
+
+	public String getISIN() {
+		return ISIN;
+	}
+
+	public void setISIN(String iSIN) {
+		ISIN = iSIN;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getBulSellIndicator() {
+		return bulSellIndicator;
+	}
+
+	public void setBulSellIndicator(String bulSellIndicator) {
+		this.bulSellIndicator = bulSellIndicator;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getExchangeName() {
+		return exchangeName;
+	}
+
+	public void setExchangeName(String exchangeName) {
+		this.exchangeName = exchangeName;
+	}
+	
+	
+	
+	
 }
