ba5a286efe99add4df6db168d325f72aad33c3ef
RangaReddyV <36688572+RangaReddyV@users.noreply.github.com>
2019-11-09 11:23:42 +0530

    first commit

---
+++ hack2hire/src/main/java/com/dbs/hack2hire/hack2hire/Hack2hireApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.hack2hire.hack2hire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hack2hireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hack2hireApplication.class, args);
+	}
+
+}
---
+++ hack2hire/src/test/java/com/dbs/hack2hire/hack2hire/Hack2hireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbs.hack2hire.hack2hire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hack2hireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
